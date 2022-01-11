29c75b76a157e3e793515f17e5335da9f4d51c0c
Willy Vargas <wamdev01@gmail.com>
2022-01-11 16:26:32 -0400

    fixs 1

---
+++ application/logs/log-2022-01-12.php
@@ -0,0 +1,38 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-01-12 01:48:41 --> Could not find the language line "hello"
+ERROR - 2022-01-12 01:48:41 --> Could not find the language line "user_id"
+ERROR - 2022-01-12 01:48:41 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\savsoftquiz_v5\application\views\view_result.php 356
+ERROR - 2022-01-12 01:49:54 --> Severity: Warning --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 25, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() C:\xampp\htdocs\savsoftquiz_v5\system\libraries\Email.php 1896
+ERROR - 2022-01-12 01:49:54 --> Could not find the language line "hello"
+ERROR - 2022-01-12 01:49:54 --> Could not find the language line "user_id"
+ERROR - 2022-01-12 01:49:54 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\savsoftquiz_v5\application\views\view_result.php 356
+ERROR - 2022-01-12 01:50:23 --> Could not find the language line "hello"
+ERROR - 2022-01-12 01:50:23 --> Could not find the language line "user_id"
+ERROR - 2022-01-12 01:50:23 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\savsoftquiz_v5\application\views\view_result.php 356
+ERROR - 2022-01-12 01:50:31 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'where qid='160'' at line 1 - Invalid query: update savsoft_qbank set    where qid='160'  
+ERROR - 2022-01-12 01:50:34 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'where qid='159'' at line 1 - Invalid query: update savsoft_qbank set    where qid='159'  
+ERROR - 2022-01-12 01:50:37 --> Could not find the language line "hello"
+ERROR - 2022-01-12 01:50:37 --> Could not find the language line "user_id"
+ERROR - 2022-01-12 01:50:37 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\savsoftquiz_v5\application\views\view_result.php 356
+ERROR - 2022-01-12 01:52:58 --> Could not find the language line "hello"
+ERROR - 2022-01-12 01:52:58 --> Could not find the language line "user_id"
+ERROR - 2022-01-12 01:52:58 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\savsoftquiz_v5\application\views\view_result.php 356
+ERROR - 2022-01-12 01:53:29 --> Could not find the language line "hello"
+ERROR - 2022-01-12 01:53:29 --> Could not find the language line "user_id"
+ERROR - 2022-01-12 01:53:29 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\savsoftquiz_v5\application\views\view_result.php 356
+ERROR - 2022-01-12 01:53:33 --> Could not find the language line "hello"
+ERROR - 2022-01-12 01:53:33 --> Could not find the language line "user_id"
+ERROR - 2022-01-12 01:53:33 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\savsoftquiz_v5\application\views\view_result.php 356
+ERROR - 2022-01-12 01:54:54 --> Could not find the language line "hello"
+ERROR - 2022-01-12 01:54:54 --> Could not find the language line "user_id"
+ERROR - 2022-01-12 01:54:54 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\savsoftquiz_v5\application\views\view_result.php 357
+ERROR - 2022-01-12 01:55:05 --> Could not find the language line "hello"
+ERROR - 2022-01-12 01:55:05 --> Could not find the language line "user_id"
+ERROR - 2022-01-12 01:55:05 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\savsoftquiz_v5\application\views\view_result.php 357
+ERROR - 2022-01-12 01:55:45 --> Could not find the language line "hello"
+ERROR - 2022-01-12 01:55:45 --> Could not find the language line "user_id"
+ERROR - 2022-01-12 01:55:45 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\savsoftquiz_v5\application\views\view_result.php 357
+ERROR - 2022-01-12 01:56:02 --> Could not find the language line "hello"
+ERROR - 2022-01-12 01:56:02 --> Could not find the language line "user_id"
+ERROR - 2022-01-12 01:56:02 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\savsoftquiz_v5\application\views\view_result.php 357
