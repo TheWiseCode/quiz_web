1ecf71eaf524813ef70aede7d77d3f996bd8ceff
vegapamela <vfpamela95@gmail.com>
2022-01-14 09:04:54 -0400

    WIP

--- application/logs/log-2022-01-13.php
+++ application/logs/log-2022-01-13.php
@@ -12,3 +12,5 @@ ERROR - 2022-01-13 02:17:48 --> Severity: error --> Exception: syntax error, une
 ERROR - 2022-01-13 02:18:30 --> Severity: error --> Exception: syntax error, unexpected 'isValid' (T_STRING), expecting function (T_FUNCTION) C:\xampp\htdocs\quiz_web\application\models\User_model.php 803
 ERROR - 2022-01-13 02:18:45 --> Severity: error --> Exception: syntax error, unexpected 'isValid' (T_STRING), expecting function (T_FUNCTION) C:\xampp\htdocs\quiz_web\application\models\User_model.php 803
 ERROR - 2022-01-13 02:27:32 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\quiz_web\application\models\User_model.php 452
+ERROR - 2022-01-13 07:08:01 --> Severity: Warning --> Division by zero C:\xampp\htdocs\quiz_web\application\views\dashboard.php 237
+ERROR - 2022-01-13 07:11:47 --> Severity: Warning --> Division by zero C:\xampp\htdocs\quiz_web\application\views\dashboard.php 237
