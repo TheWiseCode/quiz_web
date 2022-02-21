319407e060dcf4f53edb51db57cd27ad4e748bb3
Willy Vargas <wamdev01@gmail.com>
2022-02-20 23:27:13 -0400

    wip

---
+++ application/logs/log-2022-02-20.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-02-20 23:25:18 --> Severity: Warning --> Division by zero C:\xampp\htdocs\cridai_quiz\application\views\dashboard.php 198
+ERROR - 2022-02-20 23:25:43 --> Severity: Warning --> Division by zero C:\xampp\htdocs\cridai_quiz\application\views\dashboard.php 198
